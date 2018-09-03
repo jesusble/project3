96d3a42f70675cc1d9a8eb8a20a25191ebf8193c
jesusble <keziahblessy33@gmail.com>
2018-09-03 13:40:05 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Even
+{
+public static void main(String args[])
+{
+System.out.println("enter two values");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+for(i=a;i<=a;i++)
+{
+for(j=b;j<=b;j++)
+{
+if((a%2==0)&&(b%2==0))
+{
+System.out.println(a,b)
+}
+else
+{
+System.out.println(a,b);
+}
+}
+}
+}
+}
